4432c92bdee4267e49a3d06932f495d1cc0f7d99
AntonGulak <antongulak7@gmail.com>
2021-06-05 14:50:17 +0900

    client

--- client/src/chat/client/ClientWindow.java
+++ client/src/chat/client/ClientWindow.java
@@ -1,7 +1,42 @@
 package chat.client;
 
-public class ClientWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientWindow extends JFrame {
+
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+        private static final int HEIGHT = 400;
+
     public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickName = new JTextField("alex");
+    private JTextField fieldInput = new JTextField();
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickName, BorderLayout.NORTH);
+
+        setVisible(true);
 
     }
 }
